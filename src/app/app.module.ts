bb8bcdc2d04dc8385881b1b2b6870f1065b01893
NishaMishra <am474002@gmail.com>
2019-08-13 14:43:21 +0530

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {CommonModule} from '@angular/common'
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
+import { Ng2OrderModule } from 'ng2-order-pipe';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatSelectModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    Ng2OrderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/entities/product.entities.ts
@@ -0,0 +1,13 @@
+export class ProductList{
+    id:number;
+    name:string;
+    price:number;
+    subCategoryId:number;
+    categoryId:number;
+    rate:number;
+    content:string;
+    review:number;
+    typeVariant:string;
+    colorVariant:string;
+    imageUrl:string;
+}
